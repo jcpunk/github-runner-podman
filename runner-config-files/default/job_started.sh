c606f78c49a6bb816fc2d4a696cfd09f14254398
Pat Riehecky <riehecky@fnal.gov>
2024-04-16 13:26:42 -0500

    Try to fixup podman

--- runner-config-files/default/job_started.sh
+++ runner-config-files/default/job_started.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 if [[ ! -e /usr/bin/podman ]]; then
-    sudo apt-get update
-    sudo apt-get install -y podman
+    echo | sudo -S apt-get update
+    echo | sudo -S apt-get install -y podman
 fi
