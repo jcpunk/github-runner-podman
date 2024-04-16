3e61f7a1d88658816b4b996118ac92b3717a2811
Pat Riehecky <riehecky@fnal.gov>
2024-04-16 15:14:07 -0500

    Try to setup podman with socket

--- runner-config-files/default/job_started.sh
+++ runner-config-files/default/job_started.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
 if [[ ! -e /usr/bin/podman ]]; then
+    echo "Installing podman"
     echo | sudo -S apt-get update
     echo | sudo -S apt-get install -y podman
+fi
+
+if [[ ! -e ~/.config/containers/containers.conf ]]; then
+    echo "Configuring podman"
+    mkdir -p ~/.config/containers
+    cat <<EOF >~/.config/containers/containers.conf
+[engine]
+  remote = true
+  active_service = "docker"
+  [engine.service_destinations]
+    [engine.service_destinations.docker]
+      uri = "unix:///run/docker.sock"
+EOF
 fi
